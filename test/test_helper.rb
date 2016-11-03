d3353a7f17fdad2677efdc4ebb79e09e4c54946a
Maya Wang <salome.neptune@gmail.com>
2016-11-03 16:55:50 -0700

    change require position in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
-require 'vcr'
-require 'webmock/minitest'
-
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'vcr'
+require 'webmock/minitest'
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
